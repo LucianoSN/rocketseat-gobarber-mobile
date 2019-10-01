2deec8c62e6db8c38abff40172667619e5ae5ce9
Luciano de Souza Nery <lsnery@gmail.com>
2019-10-01 18:58:26 -0300

    configurando background

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
-import { View } from 'react-native';
+import { Text } from 'react-native';
+
+import Background from '~/components/Background';
 
 // import {Container} from './styles'
 
 const SignUp = () => {
-	return <View></View>;
+	return (
+		<Background>
+			<Text>SigUp</Text>
+		</Background>
+	);
 };
 
 export default SignUp;
