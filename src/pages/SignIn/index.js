e9ffabf4186c4c9300aa9f56005dd75d32590d18
Luciano de Souza Nery <lsnery@gmail.com>
2019-10-04 17:49:51 -0300

    página signin

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,22 +1,47 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Image } from 'react-native';
 
-import Input from '~/components/Input';
-import Button from '~/components/Button';
+import logo from '~/assets/logo.png';
+
 import Background from '~/components/Background';
 
-// import {Container} from './styles'
+import {
+	Container,
+	Form,
+	FormInput,
+	SubmitButton,
+	SignLink,
+	SignLinkText,
+} from './styles';
 
 const SignIn = () => {
 	return (
 		<Background>
-			<Text>SigIn</Text>
-			<Input
-				style={{ marginTop: 30 }}
-				icon="call"
-				placeholder="Digite seu nome"
-			/>
-			<Button>Entrar</Button>
+			<Container>
+				<Image source={logo} />
+
+				<Form>
+					<FormInput
+						icon="mail-outline"
+						keyboardType="email-address"
+						autoCorrect={false}
+						autoCapitalize="none"
+						placeholder="Digite seu e-mail"
+					/>
+
+					<FormInput
+						icon="lock-outline"
+						secureTextEntry
+						placeholder="Sua senha secreta"
+					/>
+
+					<SubmitButton onPress={() => {}}>Acessar</SubmitButton>
+
+					<SignLink onPress={() => {}}>
+						<SignLinkText>Criar conta gratuita</SignLinkText>
+					</SignLink>
+				</Form>
+			</Container>
 		</Background>
 	);
 };
