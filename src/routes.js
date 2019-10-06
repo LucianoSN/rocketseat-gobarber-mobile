69e9e8b110ba0fe0a834c3cc9fd4f7aaabb7b904
Luciano de Souza Nery <lsnery@gmail.com>
2019-10-06 13:03:16 -0300

    rota inicial

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,32 @@
 import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
 
 import SignIn from '~/pages/SignIn';
 import SignUp from '~/pages/SignUp';
 
-export default createAppContainer(
-	createSwitchNavigator({
-		SignIn,
-		SignUp,
-	})
-);
+import Dashboard from '~/pages/Dashboard';
+
+/*
+cd android
+gradlew clean
+cd ..
+react - native run-android
+*/
+
+export default (isSigned = false) =>
+	createAppContainer(
+		createSwitchNavigator(
+			{
+				Sign: createSwitchNavigator({
+					SignIn,
+					SignUp,
+				}),
+				App: createBottomTabNavigator({
+					Dashboard,
+				}),
+			},
+			{
+				initialRouteName: isSigned ? 'App' : 'Sign',
+			}
+		)
+	);
