ee8cedb02a02c5f24b27ab5b952b058cf478cac6
Luciano de Souza Nery <lsnery@gmail.com>
2019-10-31 18:48:28 -0300

    agendamentos da api

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,18 +1,27 @@
-import React from 'react';
+import React, { useMemo } from 'react';
+import { parseISO, formatRelative } from 'date-fns';
+import pt from 'date-fns/locale/pt-BR';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import { Container, Left, Avatar, Info, Name, Time } from './styles';
 
-const Appointment = ({ data }) => {
+const Appointment = ({ data, onCancel }) => {
 	let avatar_url = data.provider.avatar.url;
 
 	// if (__DEV__ && avatar_url !== null) {
 	// 	avatar_url = String(avatar_url).replace('localhost', '10.0.3.2');
 	// }
 
+	const dateParsed = useMemo(() => {
+		return formatRelative(parseISO(data.date), new Date(), {
+			locale: pt,
+			addSuffix: true,
+		});
+	}, [data.date]);
+
 	return (
-		<Container>
+		<Container past={data.past}>
 			<Left>
 				<Avatar
 					source={{
@@ -23,13 +32,15 @@ const Appointment = ({ data }) => {
 				/>
 				<Info>
 					<Name>{data.provider.name}</Name>
-					<Time>em 3 horas</Time>
+					<Time>{dateParsed}</Time>
 				</Info>
 			</Left>
 
-			<TouchableOpacity onPress={() => {}}>
-				<Icon name="event-busy" size={20} color="#f64c75" />
-			</TouchableOpacity>
+			{data.cancelable && !data.canceled_at && (
+				<TouchableOpacity onPress={onCancel}>
+					<Icon name="event-busy" size={20} color="#f64c75" />
+				</TouchableOpacity>
+			)}
 		</Container>
 	);
 };
--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -19,6 +19,21 @@ const Dashboard = () => {
 		loadAppointments().then();
 	}, []);
 
+	const handleCancel = async id => {
+		const response = await api.delete(`appointments/${id}`);
+
+		setAppointments(
+			appointments.map(appointment =>
+				appointment.id === id
+					? {
+							...appointment,
+							canceled_at: response.data.canceled_at,
+					  }
+					: appointment
+			)
+		);
+	};
+
 	return (
 		<Background>
 			<Container>
@@ -27,7 +42,12 @@ const Dashboard = () => {
 				<List
 					data={appointments}
 					keyExtractor={item => String(item.id)}
-					renderItem={({ item }) => <Appointment data={item} />}
+					renderItem={({ item }) => (
+						<Appointment
+							onCancel={() => handleCancel(item.id)}
+							data={item}
+						/>
+					)}
 				/>
 			</Container>
 		</Background>
